051cba7104f4afa8fe5a352d4f13b973cd6e87ea
Calvin Wu <calvin.c.wu@gmail.com>
2023-10-12 12:16:12 -0400

    [testng] add test for test_migrate_inject_constructor (#10)
     new test - test_migrate_inject_constructor

---
+++ scripts/testng-junit/src/tests/test_migrate_inject_constructor.py
@@ -0,0 +1,41 @@
+from setup import testng2junit
+from setup import assert_equal_content
+
+content = """
+
+  private final SomeServiceA serviceA;
+    
+  private final SomeServiceB serviceB;
+    
+  @Inject
+  public SomeClass(SomeServiceA serviceA, SomeServiceB serviceB) {
+    this.serviceA = serviceA;
+    this.serviceB = serviceB;
+  }
+  
+  @Test
+  public void runSomeTest() {
+  
+  }
+
+"""
+
+expected = """
+
+  @Inject
+  private SomeServiceA serviceA;
+
+  @Inject
+  private SomeServiceB serviceB;
+
+  @Test
+  public void runSomeTest() {
+
+  }
+
+"""
+
+
+def test_migrate_inject_constructor():
+    content_new = testng2junit.migrate_inject_constructor('SomeClass', content)
+    assert_equal_content(content_new, expected)
