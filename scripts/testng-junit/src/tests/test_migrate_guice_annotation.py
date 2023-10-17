248f6c65798f746bb8c416b780656239b21eb84b
Jieyu Lu <jieyu.lu@addepar.com>
2023-10-16 23:07:45 -0700

    add test

--- scripts/testng-junit/src/tests/test_migrate_guice_annotation.py
+++ scripts/testng-junit/src/tests/test_migrate_guice_annotation.py
@@ -2,7 +2,7 @@ from setup import testng2junit
 from setup import assert_equal_content
 
 
-content = """
+content_1 = """
 
     @Guice(modules = SomeModule.class)
     public class SomeTest {
@@ -15,7 +15,7 @@ content = """
 
 """
 
-expected = """
+expected_1 = """
 
     public class SomeTest {
     
@@ -28,7 +28,61 @@ expected = """
     }
 """
 
+content_2 = """
+
+    @Guice
+    public class SomeTest {
+
+      @Before
+      public void someTest() {
+
+      }
+    }
+
+"""
+
+expected_2 = """
+
+    public class SomeTest {
+
+      private final Injector injector = Guice.createInjector();
+
+      @Before
+      public void someTest() {
+        injector.injectMembers(this);
+      }
+    }
+"""
+
+content_3 = """
+
+    @Guice(modules = {TestModuleA.class,
+        Test.ModuleB.class})
+    public class SomeTest {
+
+      @Before
+      public void someTest() {
+
+      }
+    }
+
+"""
+
+expected_3 = """
+
+    public class SomeTest {
+
+      private final Injector injector = Guice.createInjector(new TestModuleA(), new Test.ModuleB());
+
+      @Before
+      public void someTest() {
+        injector.injectMembers(this);
+      }
+    }
+"""
+
 
 def test_migrate_guice_annotation():
-    content_new = testng2junit.migrate_guice_annotation(content)
-    assert_equal_content(content_new, expected)
+    assert_equal_content(testng2junit.migrate_guice_annotation(content_1), expected_1)
+    assert_equal_content(testng2junit.migrate_guice_annotation(content_2), expected_2)
+    assert_equal_content(testng2junit.migrate_guice_annotation(content_3), expected_3)
